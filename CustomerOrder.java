926b2cd529021e57bec0e5ec3c5cbf6c2cd801fe
Bryan <bryan.ferrando@epitech.eu>
2023-11-06 17:53:12 +0000

    Marvin

--- CustomerOrder.java
+++ CustomerOrder.java
@@ -1,132 +1,127 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerOrder {
 
-    private List<Menu<?, ?>> menus = new ArrayList<>();
-    private Map<Class<? extends Food>, Integer> order = new HashMap<>();
-    private Stock stock;
-    public CustomerOrder(Stock stock){
+    private final Stock stock;
+    private final List<Food> order = new ArrayList<>();
+    private final List<Menu<?, ?>> menu = new ArrayList<>();
+
+    public CustomerOrder(Stock stock) {
         this.stock = stock;
     }
 
-    public boolean addItem(Food food){
+    public boolean addItem(Food food) {
         try {
-            stock.remove(food.getClass());
-            if(!order.containsKey(food.getClass())){
-                order.put(food.getClass(), 1);
-            } else {
-                order.put(food.getClass(), order.get(food.getClass()) + 1);
-            }
-        } catch (NoSuchFoodException e) {
-            return false;
-        }
-        return true;
-    }
-
-    public boolean removeItem(Food food){
-        try {
-            stock.add(food.getClass());
-            if(!order.containsKey(food.getClass())){
+            if (stock.getNumberOf(food.getClass()) == 0) {
                 return false;
-            } else {
-                order.put(food.getClass(), order.get(food.getClass()) - 1);
             }
+            int current_order = order.size();
+            order.add(food);
+            return order.size() == current_order + 1 && stock.remove(food.getClass());
         } catch (NoSuchFoodException e) {
             return false;
         }
-        return true;
     }
 
-    public float getPrice(){
-        float price = 0;
-        for (Class<? extends Food> food : order.keySet()) {
-            try {
-                price += food.getDeclaredConstructor().newInstance().getPrice() * order.get(food);
-            } catch (Exception e) {
-                e.printStackTrace();
+    public boolean removeItem(Food food) {
+        try {
+            if (stock.getNumberOf(food.getClass()) == 0) {
+                return false;
             }
+            int current_order = order.size();
+            order.remove(food);
+            return order.size() == current_order - 1 && stock.add(food.getClass());
+        } catch (NoSuchFoodException e) {
+            return false;
+        }
+    }
+
+    public float getPrice() {
+        float price = 0;
+        for (Food food : order) {
+            price += food.getPrice();
+        }
+        for (Menu<?, ?> menu : this.menu) {
+            price += menu.getPrice();
         }
         return price;
     }
 
-    public boolean addMenu(Menu<?, ?> menu) throws NoSuchFoodException {
-        boolean drink = true;
+    public boolean addMenu(Menu<? extends Food, ? extends Food> menu) {
         try {
-            drink = stock.remove(menu.getDrink().getClass());
-            stock.remove(menu.getMeal().getClass());
-            menus.add(menu);
-        } catch (NoSuchFoodException e){
-            if(!drink){
-                stock.remove(menu.getDrink().getClass());
+            if (stock.getNumberOf(menu.getDrink().getClass()) == 0 || stock.getNumberOf(menu.getMeal().getClass()) == 0) {
+                return false;
             }
-            throw new NoSuchFoodException(e.getMessage());
-        }
-        return true;
-    }
 
-    public boolean removeMenu(Menu<?, ?> menu) throws NoSuchFoodException {
-        if(!menus.contains(menu)){
+            stock.remove(menu.getDrink().getClass());
+            stock.remove(menu.getMeal().getClass());
+
+            this.menu.add(menu);
+            return true;
+
+        } catch (NoSuchFoodException e) {
+
             return false;
         }
-
-        boolean drink = true;
-        try {
-            drink = stock.add(menu.getDrink().getClass());
-            stock.add(menu.getMeal().getClass());
-            menus.remove(menu);
-        } catch (NoSuchFoodException e){
-            if(!drink){
-                stock.add(menu.getDrink().getClass());
-            }
-
-            throw new NoSuchFoodException(e.getMessage());
-        }
-        return true;
     }
 
-    public void printOrder(){
-        float price = 0;
-        StringBuilder sb = new StringBuilder();
-        sb.append("Your order is composed of:\n");
+    public boolean removeMenu(Menu<? extends Food, ? extends Food> menu) {
+        try {
+            if (this.menu.contains(menu)) {
+                stock.add(menu.getDrink().getClass());
+                stock.add(menu.getMeal().getClass());
 
-        for(Menu<?, ?> menu : menus){
-            price += menu.getPrice();
-            sb.append("- ")
-                    .append(menu.getClass().getName())
-                    .append(" menu (")
-                    .append(menu.getPrice())
-                    .append(" euros)")
-                    .append("\n")
-                    .append("-> drink: ")
-                    .append(menu.getDrink().getClass().getName())
-                    .append("\n")
-                    .append("-> meal: ")
-                    .append(menu.getMeal().getClass().getName())
-                    .append("\n");
+                this.menu.remove(menu);
+                return true;
+            }
+            return false;
+        } catch (NoSuchFoodException e) {
+            return false;
+        }
+    }
+
+    public void printOrder() {
+        float totalPrice = 0;
+
+        System.out.println("Your order is composed of:");
+
+         for (Menu<?, ?> item : menu) {
+            Menu<? extends Food, ? extends Food> menu = item;
+
+            String menuName = menu.getClass().getSimpleName() + " menu";
+            float menuPrice = menu.getPrice();
+
+            System.out.println("- " + menuName + " (" + menuPrice + " euros)");
+            System.out.println("-> drink: " + menu.getDrink().getClass().getSimpleName());
+            System.out.println("-> meal: " + menu.getMeal().getClass().getSimpleName());
+
+            totalPrice += menuPrice;
         }
 
-        for(Map.Entry<Class<? extends Food>, Integer> food : order.entrySet()){
-            for(int i = 0; i < food.getValue(); i++){
-                try {
-                    float p = food.getKey().getDeclaredConstructor().newInstance().getPrice();
-                    price += p;
-                    sb.append("- ")
-                            .append(food.getKey().getName())
-                            .append(" (")
-                            .append(p)
-                            .append(" euros)")
-                            .append("\n");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        for (Food item : order) {
+            if (item instanceof Menu<?, ?>) {
+                Menu<? extends Food, ? extends Food> menu = (Menu<? extends Food, ? extends Food>) item;
+
+                String menuName = menu.getClass().getSimpleName() + " menu";
+                float menuPrice = menu.getPrice();
+
+                System.out.println("- " + menuName + " (" + menuPrice + " euros)");
+                System.out.println("-> drink: " + menu.getDrink().getClass().getSimpleName());
+                System.out.println("-> meal: " + menu.getMeal().getClass().getSimpleName());
+
+                totalPrice += menuPrice;
+            } else {
+                String itemName = item.getClass().getSimpleName();
+                float itemPrice = item.getPrice();
+
+                System.out.println("- " + itemName + " (" + itemPrice + " euros)");
+
+                totalPrice += itemPrice;
             }
         }
 
-        sb.append("For a total of ")
-                .append(price)
-                .append(" euros.");
-
-        System.out.println(sb.toString());
+        System.out.println("For a total of " + totalPrice + " euros.");
     }
 
 }
